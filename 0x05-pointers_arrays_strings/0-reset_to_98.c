8a54f2b2d98bebc1e984ac0769e3fdb684efcd64
minato18171 <allahyaniamina@gmail.com>
2023-09-18 13:54:17 -0700

    hbg

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+
+
+int n;
+void reset_to_98(int *n)
+{
+
+    *n = 98
+{
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+void reset_to_98(int *n);
