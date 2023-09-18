b1ffa7cc7b63f721625c39dd27df78453f11a50b
minato18171 <allahyaniamina@gmail.com>
2023-09-18 14:49:36 -0700

    huv

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,9 +3,13 @@
 
 /**
  * File: main.h
+ * Auth: JohnKamau
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the 0x04-pointers_arrays_strings directory.
  */
 
 #include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
+#endif
