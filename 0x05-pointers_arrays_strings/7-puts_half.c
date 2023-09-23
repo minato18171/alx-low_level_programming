5675cf4a5e1d2ec55ff42b190782d9f90ea8ab77
minato18171 <allahyaniamina@gmail.com>
2023-09-22 17:28:55 -0700

    h

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,18 +2,16 @@
 void puts_half(char *str)
 {
 int length_of_the_string=0;
-int m=0;
 int i=0;
 int indice=0;
 while (str[indice++]){
      length_of_the_string++; 
 }
 for (i=0;i<=length_of_the_string/2;i++ ){
-m=length_of_the_string/2;
 if(length_of_the_string%2==0){
-    putchar(str[i]);
+    _putchar(str[i]);
 }else if (length_of_the_string % 2 != 0){
-    putchar(str[(length_of_the_string-1)/2]);
+    _putchar(str[(length_of_the_string-1)/2]);
 }
 }
 }
