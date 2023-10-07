fbce899a652dab28902899a508d1fd04f7a02f5d
minato18171 <allahyaniamina@gmail.com>
2023-10-07 08:19:46 -0700

    h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars..
+ * @size: size.
+ * @c: array.
+ * Return: Returns a pointer to the array, or NULL if it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *y = malloc(size)
+	if (size ==0)
+	{ 
+		return(0);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+malloc, free , and memory allocation
